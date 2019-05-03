23059f660eceae530eea9d529e3c8f8278f47d68
Samuel <samueljosiahward@gmail.com>
2019-05-04 11:33:34 +1200

    HomePage Activity -- Base

--- app/src/main/java/com/example/dailyhealth/HomePageActivity.java
+++ app/src/main/java/com/example/dailyhealth/HomePageActivity.java
@@ -3,11 +3,11 @@ package com.example.dailyhealth;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HomePageActivity extends AppCompatActivity {
 
 @Override
 protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_homepage);
         }
         }
