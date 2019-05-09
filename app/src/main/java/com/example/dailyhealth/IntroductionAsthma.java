0edc1451683cc081e03f31b8de208b37e6140b74
Samuel <samueljosiahward@gmail.com>
2019-05-10 09:58:56 +1200

    Asthma Introduction -- Java File XML File

---
+++ app/src/main/java/com/example/dailyhealth/IntroductionAsthma.java
@@ -0,0 +1,13 @@
+package com.example.dailyhealth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroductionAsthma extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction_asthma);
+    }
+}
