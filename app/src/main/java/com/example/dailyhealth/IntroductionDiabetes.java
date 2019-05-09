a3aa0ed7e586a914feac050b1a7b9d33f236a52e
Samuel <samueljosiahward@gmail.com>
2019-05-10 10:54:12 +1200

    Disease Intro: Diabetes -- Java File XML File

---
+++ app/src/main/java/com/example/dailyhealth/IntroductionDiabetes.java
@@ -0,0 +1,13 @@
+package com.example.dailyhealth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroductionDiabetes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction_diabetes);
+    }
+}
