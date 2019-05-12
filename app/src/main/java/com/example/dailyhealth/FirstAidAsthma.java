86935a8e467ef21be25f99de94e5d51dfa695660
Samuel <samueljosiahward@gmail.com>
2019-05-12 14:12:31 +1200

    FirstAidAsthma + FirstAidDiabetes -- Navigation set

--- app/src/main/java/com/example/dailyhealth/FirstAidAsthma.java
+++ app/src/main/java/com/example/dailyhealth/FirstAidAsthma.java
@@ -2,6 +2,8 @@ package com.example.dailyhealth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class FirstAidAsthma extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class FirstAidAsthma extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_aid_asthma);
+
+        //Setting Views
+        final ImageView backArrowButton = findViewById(R.id.icon_back_arrow);
+
+        //Setting Navigation
+        //Back Arrow
+        backArrowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backArrowButton.setColorFilter(R.color.colorDBlue);
+                finish();
+            }
+        });
     }
 }
