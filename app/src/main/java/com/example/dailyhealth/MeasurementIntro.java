90ce13461f13c1daa55a38948d9288fc60e44ce6
Samuel <samueljosiahward@gmail.com>
2019-05-12 14:18:26 +1200

    MeasurementIntro -- Navigation set (validation pending)

--- app/src/main/java/com/example/dailyhealth/MeasurementIntro.java
+++ app/src/main/java/com/example/dailyhealth/MeasurementIntro.java
@@ -1,7 +1,10 @@
 package com.example.dailyhealth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MeasurementIntro extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MeasurementIntro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_measurement_intro);
+
+        //Setting Views
+        final Button doItNowButton = findViewById(R.id.btn_measurement_yes);
+        final Button doItLaterButton = findViewById(R.id.btn_measurement_no);
+
+        //Setting Navigation
+        //Do It Now Button
+        doItNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MeasurementIntro.this, MeasurementPage1.class);
+                startActivity(i);
+            }
+        });
+
+        //Do It Later Button
+        doItLaterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
